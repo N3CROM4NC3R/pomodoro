c2491744d24662ad9ca833820f4b92efa1811dbd
N3CROM4NC3R <santi16648@gmail.com>
2023-03-31 14:59:57 -0400

    style: custom dropdown menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,8 +62,8 @@
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                <div class="dropdown-menu dropdown-menu-end custom-dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item custom-dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
